56e20c26f4f814a35a9dc2ff1e1009d7dd8b3e75
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2023-11-05 14:50:30 +1100

    added integrations

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import BannerImg from "../assets/banner.png";
+import Integrations from "./Integrations";
 
 const Banner = () => {
   return (
-    <div className="flex flex-col lg:flex-row gap-16">
-      <div className="w-full lg:w-6/12 flex flex-col gap-8 lg:gap-12">
-        <h1>Refine and improve your sales with AI</h1>
-        <h4>
-          Do you want to boost revenue and make predictions with accuracy?
-        </h4>
-        <button className="bg-bg-cta text-white w-fit">
-          <a href="#onboard">Join the waitlist</a>
-        </button>
+    <div>
+      <div className="flex flex-col lg:flex-row gap-16">
+        <div className="w-full lg:w-6/12 flex flex-col gap-8 lg:gap-12">
+          <h1>Refine and improve your sales with AI</h1>
+          <h4 className="text-font-secondary">
+            Do you want to boost revenue and make predictions with accuracy?
+          </h4>
+          <button className="bg-bg-cta text-white w-fit">
+            <a href="#onboard">Join the waitlist</a>
+          </button>
+        </div>
+        <div className="w-full lg:w-6/12 shadow-xl">
+          <img src={BannerImg} alt="FlyKick UI" className="w-full h-full" />
+        </div>
       </div>
-      <div className="w-full lg:w-6/12 shadow-xl">
-        <img src={BannerImg} alt="FlyKick UI" className="w-full h-full" />
+      <div className="mt-16 lg:mt-32">
+        <Integrations />
       </div>
     </div>
   );
---
+++ frontend/src/components/Integrations.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { FaSalesforce } from "react-icons/fa";
+import adwords from "../assets/companies/adwords.png";
+import gmail from "../assets/companies/gmail.png";
+import hubspot from "../assets/companies/hubspot.png";
+import outlook from "../assets/companies/outlook.png";
+import salesforce from "../assets/companies/salesforce.png";
+import slack from "../assets/companies/slack.png";
+import teams from "../assets/companies/teams.svg";
+import zoom from "../assets/companies/zoom.png";
+
+const Integrations = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-4 md:grid-cols-4 gap-8 items-center place-items-center">
+        <img src={adwords} alt="adwords" className="w-8 md:w-16" />
+        <img src={gmail} alt="gmail" className="w-8 md:w-16" />
+        <img src={hubspot} alt="hubspot" className="w-8 md:w-16" />
+        <img src={outlook} alt="outlook" className="w-8 md:w-16" />
+        <img src={salesforce} alt="salesforce" className="w-8 md:w-16" />
+        <img src={slack} alt="slack" className="w-8 md:w-16" />
+        <img src={teams} alt="teams" className="w-8 md:w-16" />
+        <img src={zoom} alt="zoom" className="w-8 md:w-16" />
+      </div>
+    </div>
+  );
+};
+
+export default Integrations;
