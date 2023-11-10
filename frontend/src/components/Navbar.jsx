25c273cf7e6e70e01679e9ca427784099aa5df96
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2023-11-10 11:43:33 +1100

    added smooth scroll from privacy policy

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FiMenu } from "react-icons/fi";
 import Logo from "../assets/logo.png";
 import { AiOutlineClose, AiOutlineRight } from "react-icons/ai";
@@ -6,6 +6,24 @@ import { motion } from "framer-motion";
 
 const Navbar = () => {
   const [showMobileNav, setShowMobileNav] = useState(false);
+
+  const scrollToSection = (sectionId) => {
+    const targetElement = document.querySelector(sectionId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+      setShowMobileNav(false);
+    }
+  };
+
+  useEffect(() => {
+    // Handle hash-based navigation if there is a hash in the URL
+    const hash = window.location.hash;
+    if (hash) {
+      scrollToSection(hash);
+    }
+  }, []);
+
   return (
     <div className="mx-auto px-4 xl:px-12 max-w-7xl flex justify-between items-center h-16 md:h-20">
       <motion.a
@@ -37,25 +55,19 @@ const Navbar = () => {
           </div>
           <ul className="flex flex-col justify-center items-center h-full gap-4 px-4 font-semibold text-font-primary">
             <li
-              className="flex justify-between items-center w-full border-b pb-4"
-              onClick={() => setShowMobileNav(false)}
+              className="flex justify between items-center w-full border-b pb-4"
+              onClick={() => scrollToSection("#benefits")}
             >
-              <a
-                href="#benefits"
-                className="flex justify-between w-full items-center"
-              >
+              <a className="flex justify-between w-full items-center">
                 <h5>Benefits</h5>
                 <AiOutlineRight className="text-lg" />
               </a>
             </li>
             <li
               className="flex justify-between items-center w-full border-b pb-4"
-              onClick={() => setShowMobileNav(false)}
+              onClick={() => scrollToSection("#features")}
             >
-              <a
-                href="#features"
-                className="flex justify-between w-full items-center"
-              >
+              <a className="flex justify-between w-full items-center">
                 <h5>Features</h5>
                 <AiOutlineRight className="text-lg" />
               </a>
@@ -63,12 +75,9 @@ const Navbar = () => {
 
             <li
               className="flex justify-between items-center text-center w-full border-b pb-4"
-              onClick={() => setShowMobileNav(false)}
+              onClick={() => scrollToSection("#reviews")}
             >
-              <a
-                href="#reviews"
-                className="flex justify-between w-full items-center"
-              >
+              <a className="flex justify-between w-full items-center">
                 <h5>Reviews</h5>
                 <AiOutlineRight className="text-lg" />
               </a>
@@ -77,7 +86,7 @@ const Navbar = () => {
               <a href="#onboard">
                 <button
                   className="bg-bg-cta text-white w-full hover:bg-white hover:text-bg-cta border border-bg-cta transition-all duration-150"
-                  onClick={() => setShowMobileNav(false)}
+                  onClick={() => scrollToSection("#onboard")}
                 >
                   Join the waitlist
                 </button>
@@ -106,7 +115,7 @@ const Navbar = () => {
             viewport={{ once: true }}
             transition={{ type: "just", duration: 0.5, delay: 0.1 }}
           >
-            <a href="#benefits">
+            <a href="/#benefits">
               <h5>Benefits</h5>
             </a>
           </motion.li>
@@ -118,7 +127,7 @@ const Navbar = () => {
             viewport={{ once: true }}
             transition={{ type: "just", duration: 0.5, delay: 0.2 }}
           >
-            <a href="#features">
+            <a href="/#features">
               <h5>Features</h5>
             </a>
           </motion.li>
@@ -130,7 +139,7 @@ const Navbar = () => {
             viewport={{ once: true }}
             transition={{ type: "just", duration: 0.5, delay: 0.3 }}
           >
-            <a href="#reviews">
+            <a href="/#reviews">
               <h5>Reviews</h5>
             </a>
           </motion.li>
@@ -140,7 +149,7 @@ const Navbar = () => {
             viewport={{ once: true }}
             transition={{ type: "just", duration: 0.5, delay: 0.4 }}
           >
-            <a href="#onboard">
+            <a href="/#onboard">
               <button className="bg-bg-cta text-white hover:bg-white hover:text-bg-cta border border-bg-cta transition-all duration-150">
                 Join the waitlist
               </button>
